40e3c36ed28d3b17294406fe162bd461a1e708b0
Amlal El Mahrouss <amlal.elmahrouss@icloud.com>
2025-03-04 09:15:09 +0100

    IPCAddr.cc: Saving this change.

---
+++ dev/Kernel/src/Network/IPCAddr.cc
@@ -0,0 +1,9 @@
+/* -------------------------------------------
+
+	Copyright (C) 2024-2025, Amlal EL Mahrouss, all rights reserved.
+
+------------------------------------------- */
+
+#include <NetworkKit/IPC.h>
+#include <KernelKit/LPC.h>
+#include <KernelKit/UserProcessScheduler.h>
